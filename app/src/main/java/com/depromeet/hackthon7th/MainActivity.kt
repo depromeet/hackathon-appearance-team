8157a03af03014641f8531758142e2d0977c3eee
zion830 <zion830@honglab.org>
2019-11-22 23:00:40 +0900

    프로젝트 생성

---
+++ app/src/main/java/com/depromeet/hackthon7th/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.depromeet.hackthon7th
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
