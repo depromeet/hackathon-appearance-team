51626015ca56a31950c21b64f5699232268e67a5
cho hee <whgml1@gmail.com>
2019-11-23 20:23:32 +0900

    main Todolist

---
+++ app/src/main/java/com/depromeet/hackthon7th/main/ToDoListAdapter.java
@@ -0,0 +1,78 @@
+package com.depromeet.hackthon7th.main;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.depromeet.hackthon7th.R;
+
+import java.util.ArrayList;
+
+import static android.view.View.GONE;
+
+public class ToDoListAdapter extends RecyclerView.Adapter<ToDoHolder> {
+    private ArrayList<ToDoListItem> todoList;
+
+    public ToDoListAdapter(ArrayList<ToDoListItem> list) {
+        this.todoList = list;
+    }
+
+    @NonNull
+    @Override
+    public ToDoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        Context context = parent.getContext();
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.todo_item, parent, false);
+
+        ToDoHolder viewHolder = new ToDoHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ToDoHolder holder, int position) {
+        ToDoListItem item = todoList.get(position);
+        Log.d("adapter", item.getAlarmType());
+        if (item.getAlarmType().equals("once")) {
+            holder.alarmType.setText("특정알림");
+            holder.taskType.setVisibility(GONE);
+            if (item.getPriority().equals("high"))
+                holder.icon.setImageResource(R.drawable.ic_priority_yellow3);
+            else if (item.getPriority().equals("normal"))
+                holder.icon.setImageResource(R.drawable.ic_priority_yellow2);
+            else
+                holder.icon.setImageResource(R.drawable.ic_priority_yellow1);
+
+
+        } else {
+            holder.alarmType.setText("정기알림"); //주황
+            holder.taskType.setText(" | " + item.getAlarmType());
+            if (item.getPriority().equals("high"))
+                holder.icon.setImageResource(R.drawable.ic_priority_orange3);
+            else if (item.getPriority().equals("normal"))
+                holder.icon.setImageResource(R.drawable.ic_priority_orange2);
+            else
+                holder.icon.setImageResource(R.drawable.ic_priority_orange1);
+
+        }
+        holder.title.setText(item.getTitle());
+        if (item.getMemo().length() == 0)
+            holder.desc.setVisibility(GONE);
+        else
+            holder.desc.setText(item.getMemo());
+        holder.date.setText(item.getDate());
+        holder.time.setText(item.getTime());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return todoList.size();
+    }
+}
---
+++ app/src/main/java/com/depromeet/hackthon7th/main/ToDoListItem.java
@@ -0,0 +1,97 @@
+package com.depromeet.hackthon7th.main;
+
+public class ToDoListItem {
+    private int id;
+    private String alarmType;
+    private String taskType;
+    private String title;
+    private String desc;
+    private String date;
+    private String time;
+    private String priority;
+
+    public ToDoListItem(int id, String alarmType, String taskType, String title, String desc, String date, String time, String priority) {
+        this.id = id;
+        this.alarmType = alarmType;
+        this.taskType = taskType;
+        this.title = title;
+        this.desc = desc;
+        this.date = date;
+        this.time = time;
+        this.priority = priority;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getTaskType() {
+        return taskType;
+    }
+
+    public void setTaskType(String taskType) {
+        this.taskType = taskType;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getAlarmType() {
+        return alarmType;
+    }
+
+    public void setAlarmType(String alarmType) {
+        this.alarmType = alarmType;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMemo() {
+        return desc;
+    }
+
+    public void setMemo(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+}
