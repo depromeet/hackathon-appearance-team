c021a7f7420cec29295924933448b1abbc0f9888
zion830 <zion830@honglab.org>
2019-11-23 14:57:40 +0900

    EditText 확장함수 추가 #1

--- app/src/main/java/com/depromeet/hackthon7th/TaskDetailActivity.kt
+++ app/src/main/java/com/depromeet/hackthon7th/TaskDetailActivity.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.depromeet.hackthon7th.ext.checkLength
 import kotlinx.android.synthetic.main.activity_task_detail.*
+
 
 class TaskDetailActivity : AppCompatActivity() {
 
@@ -19,7 +22,17 @@ class TaskDetailActivity : AppCompatActivity() {
     }
 
     private fun initView() {
+        et_task_title.addTextChangedListener { text ->
+            text?.let {
+                et_task_title.checkLength(layout_task_title, text.toString())
+            }
+        }
 
+        et_task_desc.addTextChangedListener { text ->
+            text?.let {
+                et_task_desc.checkLength(layout_task_desc, text.toString())
+            }
+        }
     }
 
     companion object {
