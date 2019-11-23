953a35c927aa69e27104b7619e583b491495b1cf
zion830 <zion830@honglab.org>
2019-11-23 15:32:00 +0900

    RootApplication에 getContext 추가

--- app/src/main/java/com/depromeet/hackthon7th/RootApplication.kt
+++ app/src/main/java/com/depromeet/hackthon7th/RootApplication.kt
@@ -1,9 +1,21 @@
 package com.depromeet.hackthon7th
 
 import android.app.Application
+import android.content.Context
 import io.realm.Realm
 
 class RootApplication: Application() {
+
+    companion object {
+
+        private var APPLICATION_CONTEXT: Context? = null
+
+        @JvmStatic
+        fun getContext(): Context {
+            return APPLICATION_CONTEXT!!
+        }
+    }
+
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
