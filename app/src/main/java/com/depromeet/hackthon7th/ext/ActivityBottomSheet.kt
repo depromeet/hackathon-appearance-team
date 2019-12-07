6f0b0e0e520035a81fca014d72123f3ad5c1bd9e
zion830 <zion830@honglab.org>
2019-12-07 23:08:54 +0900

    애니메이션 적용을 위한 custom linearlayout 추가

---
+++ app/src/main/java/com/depromeet/hackthon7th/ext/ActivityBottomSheet.kt
@@ -0,0 +1,36 @@
+package com.depromeet.hackthon7th.ext
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.LinearLayout
+import com.depromeet.hackthon7th.R
+
+
+class ActivityBottomSheet @JvmOverloads
+constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : LinearLayout(
+    context, attrs, defStyleAttr
+) {
+    private val inAnimation = AnimationUtils.loadAnimation(
+        context,
+        R.anim.slide_up
+    )
+    private val outAnimation = AnimationUtils.loadAnimation(
+        context,
+        R.anim.slide_down
+    )
+
+    override fun setVisibility(visibility: Int) {
+        when (visibility) {
+            View.VISIBLE -> startAnimation(inAnimation)
+            View.INVISIBLE, View.GONE -> startAnimation(outAnimation)
+        }
+
+        super.setVisibility(visibility)
+    }
+}
